944229589bed3ead15bd3804dd87d44cbc31504b
Andy <meteochu@gmail.com>
2016-07-18 17:52:51 +0800

    Initial Commit

---
+++ WatSwift/WatSwift.h
@@ -0,0 +1,19 @@
+//
+//  WatSwift.h
+//  WatSwift
+//
+//  Created by Andy on 7/18/16.
+//  Copyright © 2016 Andy Liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WatSwift.
+FOUNDATION_EXPORT double WatSwiftVersionNumber;
+
+//! Project version string for WatSwift.
+FOUNDATION_EXPORT const unsigned char WatSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WatSwift/PublicHeader.h>
+
+
